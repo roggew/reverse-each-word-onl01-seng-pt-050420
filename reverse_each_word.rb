f8c3d74e03d95e999452b20699c8526b45d00945
William Rogge <williamroggeiv@gmail.com>
2020-05-12 20:51:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   reverse_sentence = sentence.split
-  reverse_sentence.each{|word|word.reverse!}
+  reverse_sentence.collect{|word|word.reverse!}
   reverse_sentence.join(" ")
 end
+
